fa61065705da33087a1390dc559da60e93cb48b9
Maksim <max-goryachev@ya.ru>
2023-09-19 18:44:33 +0600

    finished

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ const useValidation = (value, validations) => {
   }
 }
 
+
 const useInput = (initialValue, validations) => {
   const [value, setValue] = useState(initialValue)
   const [isDirty, setIsDirty] = useState(false)
@@ -49,26 +50,38 @@ const useInput = (initialValue, validations) => {
   }
 }
 
+
 function App() {
+  const [formValid, setFormValid] = useState(false)
   const email = useInput('', { isEmpty: true, minLengthError: 5, isEmail: true });
   const password = useInput('', { isEmpty: true, minLengthError: 3 })
+  const emptyLog = (email.isDirty && email.isEmpty) && <div className='error'>Email is Empty</div>
+  const minLength = (email.isDirty && email.minLengthError) && <div className='error'>Email is low</div>
+  const incorEmail = (email.isDirty && email.emailError) && <div className='error'>incorrect email</div>
+  const emptyPass = (password.isDirty && password.isEmpty) && <div className='error-pass'>Password is Empty</div>
+  const lowPass = (password.isDirty && password.minLengthError) && <div className='error-pass'>Password is low</div>
 
+  useEffect(() => {
+    if (incorEmail || lowPass) {
+      setFormValid(false)
+    } else {
+      setFormValid(true)
+    }
+  }, [incorEmail, lowPass])
   return (
 
     <div className="container">
       <div className='form'><h1>Account</h1>
         <img src={require('./images/tree.png')} alt="" />
-        
         {(email.isDirty && email.isEmpty) && <div className='error'>Email is Empty</div>}
-        {(email.isDirty && email.minLengthError) && <div className='error'>Email is low</div>}
-        {(email.isDirty && email.emailError) && <div className='error'>incorrect email</div>}
+        {!emptyLog ? minLength : <div className='error'></div>}
+        {!minLength ? incorEmail : <div className='error'></div>}
 
         <input onChange={e => email.onChange(e)} onBlur={e => email.onBlur(e)} value={email.value} name='email' type="text" />
-        {(password.isDirty && password.isEmpty) && <div className='error'>Password is Empty</div>}
-        {(password.isDirty && password.minLengthError) && <div className='error'>Password is low</div>}
-
+        {(password.isDirty && password.isEmpty) && <div className='error-pass'>Password is Empty</div>}
+        {!emptyPass ? lowPass : <div className='error-pass'></div>}
         <input onChange={e => password.onChange(e)} onBlur={e => password.onBlur(e)} value={password.value} name='password' type="password" />
-        <button  >Sign in</button>
+        <button disabled={!formValid}>Sign in</button>
       </div>
 
     </div>
