766da96f43916e12103a67ff6365ccced49c9120
Maksim <max-goryachev@ya.ru>
2023-09-19 14:56:12 +0600

    create a project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './styles/main.css'
+import Component from './component';
+import { useState } from 'react';
+function App() {
+  return (
+    <div>
+      <input type="text" />
+      <input type="password" />
+      <button>Registration</button>
+    </div>
+  );
+}
+
+export default App;
